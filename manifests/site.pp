14c6fe88f78e2f2fae5c7ddfca4ca5d727e2483f
Muji Sayed <MujiSayed@users.noreply.github.com>
2020-08-05 07:25:18 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,16 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'centos7-vm' {
   include role::master_server
   file {'/root/README':
     ensure  => file,
     content => "Welcome to $fqdn\n",
+    owner => 'root',
   }
 }
 node 'minetest.puppet.vm': {
