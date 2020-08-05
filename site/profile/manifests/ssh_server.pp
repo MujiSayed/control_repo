8698af998c8735461128ffcde496025eb4ccab8f
Muji Sayed <MujiSayed@users.noreply.github.com>
2020-08-05 03:28:35 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCeYKyArmtlhTzh/W37haebh8OItHMMiZ6s3pzrB7vuAalVVA5XMBB2PIZXuFS9lu/LETHA98v+CWk3hytvOfpNbtOoAYvt53vEqhEQBcIUtdhYiQOywIw9zbmNl9ZaVHg/KgcFew4YHSH4R7CoVicnNpUvA+4jusgJwozKkked0I9a69u/kvUivwNxDdF13UgkdZOBOk3ne9tzZ5qr3bGIpsBgLzvtkPqbMKT9CmZuaDzGbfYzPLiPZwq4dRFhPHzFh7uHE8TjqeJXkmVTmuoS66zla0JcP2CUBa028zWWcct7v7lhhDVCU69JQRjfCIxqa6zLUpcqZf3rcElJSOZ',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDCeYKyArmtlhTzh/W37haebh8OItHMMiZ6s3pzrB7vuAalVVA5XMBB2PIZXuFS9lu/LETHA98v+CWk3hytvOfpNbtOoAYvt53vEqhEQBcIUtdhYiQOywIw9zbmNl9ZaVHg/KgcFew4YHSH4R7CoVicnNpUvA+4jusgJwozKkked0I9a69u/kvUivwNxDdF13UgkdZOBOk3ne9tzZ5qr3bGIpsBgLzvtkPqbMKT9CmZuaDzGbfYzPLiPZwq4dRFhPHzFh7uHE8TjqeJXkmVTmuoS66zla0JcP2CUBa028zWWcct7v7lhhDVCU69JQRjfCIxqa6zLUpcqZf3rcElJSOZ',
 	}  
 }
